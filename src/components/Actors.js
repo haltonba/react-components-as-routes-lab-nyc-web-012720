25c8706f203bc91b853f6049db1e029019ac178a
Halton Bagley <haltonba94@gmail.com>
2020-04-13 03:22:46 +0000

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,24 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+
+  const renderActors = () => {
+    return actors.map(actor => {
+      return (
+        <div>
+          <h3>{actor.name}</h3>
+          <ul>
+            {actor.movies.map((movie, index) => <li key={index}>{movie}</li>)}
+          </ul>
+        </div>
+      )
+    })
+  }
+
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {renderActors()}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,25 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+
+  const renderMovies = () => {
+    return movies.map(movie => {
+      return (
+        <div>
+          <h3>{movie.title}</h3>
+          <h3>{movie.time}</h3>
+          <ul>
+            {movie.genres.map((genre, index) => <li key={index}>{genre}</li>)}
+          </ul>
+        </div>
+      )
+    })
+  }
+
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {renderMovies()}
     </div>
   );
 };
